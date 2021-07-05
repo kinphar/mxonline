8a4304cdd9502d76ca1d8087dbddfd6edda36ec4
kinphar <kinphar@163.com>
2021-07-05 10:06:36 +0800

    后台管理界面改成中文语言，且使用shanghai时区。

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -112,15 +112,16 @@ AUTH_USER_MODEL = "users.UserProfile"
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
