8585cde34eb188abd605c29807a7f231ca34e253
kinphar <kinphar@163.com>
2021-07-08 21:30:48 +0800

    完成CRM内容

--- apps/customers/adminx.py
+++ apps/customers/adminx.py
@@ -4,28 +4,30 @@ from apps.customers.models import Company, KeyPerson, Opportunity
 
 
 class CompanyAdmin(object):
-    list_display = ["name", "category", "address"]
+    list_display = ["name", "category", "address", "add_time"]
     search_fields = ["name", "category"]
     list_filter = ["name", "category"]
     list_editable = ["name", "category"]
 
 
 class KeyPersonAdmin(object):
-    list_display = ["name", "mobile", "company", "come_from"]
-    search_fields = ["name", "mobile", "company", "come_from"]
-    list_filter = ["name", "mobile", "company", "come_from"]
-    list_editable = ["name", "mobile", "company", "come_from"]
+    list_display = ["name", "mobile", "company", "position", "add_time"]
+    search_fields = ["name", "mobile", "company"]
+    list_filter = ["name", "mobile", "company"]
+    list_editable = ["name", "mobile", "company", "position"]
 
 
 class OpportunityAdmin(object):
-    list_display = ["person", "company", "product", "status"]
-    search_fields = ["person", "company", "product", "status"]
-    list_filter = ["person", "company", "product", "status"]
-    list_editable = ["status"]
+    list_display = ["company", "person", "product", "status", "add_time"]
+    search_fields = ["company", "person", "product", "status"]
+    list_filter = ["person__name", "status"]
+    list_editable = ["company", "product", "status"]
 
 
-xadmin.site.register(Company, CompanyAdmin)
-xadmin.site.register(KeyPerson, KeyPersonAdmin)
 xadmin.site.register(Opportunity, OpportunityAdmin)
+xadmin.site.register(KeyPerson, KeyPersonAdmin)
+xadmin.site.register(Company, CompanyAdmin)
+
+
 
 
--- apps/customers/models.py
+++ apps/customers/models.py
@@ -7,10 +7,9 @@ from apps.products.models import Product
 class Company(BaseModel):
     name = models.CharField(verbose_name="公司名称", max_length=50)
     address = models.CharField(verbose_name="公司地址", max_length=150)
-    desc = models.TextField(verbose_name="备注", max_length=100)
+    desc = models.TextField(verbose_name="备注", max_length=100, blank=True, default="")
     category = models.CharField(default="zd", verbose_name="类型", max_length=3,
                                 choices=(("zd", "终端客户"), ("jcs", "集成商")))
-    image = models.ImageField(upload_to="customers/%Y/%m", verbose_name="公司logo", max_length=100)
 
     class Meta:
         verbose_name = "目标公司"
@@ -23,11 +22,12 @@ class Company(BaseModel):
 class KeyPerson(BaseModel):
     name = models.CharField(verbose_name="姓名", max_length=50)
     mobile = models.CharField(verbose_name="电话", max_length=11)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="公司")
-    come_from = models.CharField(verbose_name="哪里人", max_length=50)
-    age = models.IntegerField(verbose_name="年龄", default=18)
-    desc = models.CharField(verbose_name="备注", max_length=100)
-    image = models.ImageField(upload_to="customers/%Y/%m", verbose_name="照片", max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="就职公司")
+    position = models.CharField(verbose_name="职位", max_length=50, default="未知")
+    come_from = models.CharField(verbose_name="哪里人", max_length=50, blank=True)
+    age = models.IntegerField(verbose_name="年龄", default=18, blank=True)
+    desc = models.CharField(verbose_name="备注", max_length=100, blank=True, default="")
+    image = models.ImageField(upload_to="customers/%Y/%m", verbose_name="照片", max_length=100, blank=True)
 
     class Meta:
         verbose_name = "关键人物"
@@ -42,14 +42,16 @@ STATUS_CHOICES = (
     ("bbz", "报备中"),
     ("ybb", "已报备"),
     ("bbcq", "报备超期"),
+    ("bbsb", "报备失败"),
+    ("ycj", "已成交"),
 )
 
 
 class Opportunity(BaseModel):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="购买方")
     person = models.ForeignKey(KeyPerson, on_delete=models.CASCADE, verbose_name="关键人物")
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, verbose_name="公司")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="产品")
-    status = models.CharField(default="wbb", verbose_name="报备状态", max_length=4, choices=STATUS_CHOICES)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name="产品配置")
+    status = models.CharField(default="wbb", verbose_name="状态", max_length=4, choices=STATUS_CHOICES)
 
     class Meta:
         verbose_name = "销售机会"
