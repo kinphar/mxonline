b6c1c92bde42730d0463a493005b6b5757388c62
kinphar <kinphar@163.com>
2021-07-11 16:21:06 +0800

    完善登陆页面跳转。

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "login.html")
+
+    def post(self, request, *args, **kwargs):
+        user_name = request.POST.get("username", "")
+        password = request.POST.get("password", "")
+
+        user = authenticate(username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "login.html", {"msg": "用户名或密码错误"})
+
